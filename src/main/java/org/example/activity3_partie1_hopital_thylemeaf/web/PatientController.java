d73634cbc7c7d342d0cf0a90dfe5228c1e805f2c
LinaHannouni <linahannounipro2004@gmail.com>
2025-04-14 11:38:06 +0100

    second commit

--- src/main/java/org/example/activity3_partie1_hopital_thylemeaf/web/PatientController.java
+++ src/main/java/org/example/activity3_partie1_hopital_thylemeaf/web/PatientController.java
@@ -10,10 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class PatientController {
@@ -72,6 +71,13 @@ if(bindingResult.hasErrors()){ return "formPatients";}
         model.addAttribute("keyword", keyword);
         model.addAttribute("page", page);
         return "editPatient";
+    }
+
+    @GetMapping("/index/{id}")
+    public String viewPatient(@PathVariable("id") Long id, Model model){
+        patient patient = patientRepository.findById(id).get();
+        model.addAttribute("patient", patient);
+        return "viewPatient";
     }
 
 
